12b5f58fc978718b91d1ae4d23ad3f2d57ec9f77
Junhyuck Ko <mrbartrns@naver.com>
2022-09-04 15:54:43 +0900

    solve: 프로그래머스 튜플 레벨 2

---
+++ programmers/lv2/튜플.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left, bisect_right
+
+
+def solution(s):
+    digit = []
+    arr = s[2:-2].split("},{")
+
+    for i in range(len(arr)):
+        arr[i] = arr[i].split(",")
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            digit.append(int(arr[i][j]))
+
+    digit.sort()
+    answer = [i for i in set(digit)]
+    answer.sort(key=lambda x: -(bisect_right(digit, x) - bisect_left(digit, x)))
+    return answer
