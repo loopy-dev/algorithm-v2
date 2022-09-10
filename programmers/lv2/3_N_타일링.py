4101e927e204dda9102db167bea09692465344c2
Junhyuck Ko <mrbartrns@naver.com>
2022-09-10 18:14:17 +0900

    solve: 3 n 타일링 프로그래머스 레벨 2

---
+++ programmers/lv2/3_N_타일링.py
@@ -0,0 +1,19 @@
+MOD = 1000000007
+
+
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0], dp[2] = 1, 3
+
+    for i in range(4, n + 1, 2):
+        dp[i] += dp[i - 2] * 3 % MOD
+        for j in range(4, i, 2):
+            dp[i] += (dp[i - j] * 2) % MOD
+        dp[i] += 2
+        dp[i] %= MOD
+
+    return dp[n]
+
+
+n = 4
+print(solution(n))
